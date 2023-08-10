378e633041605b43ae0355373cfc40fac5b6827c
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-10 09:07:06 -0500

    Update 7.0_Jedi_Training.py

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,7 @@
 # 7.0 Jedi Training (20pts)  Name:________________
 
 '''
-1. TEST PICTURE  (10PTS)
+1. TEST PICTURE  (10pts)
 ------------------------
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
@@ -10,7 +10,7 @@ The picture is 500px wide and 400px tall. Look up ARC in the documentation to do
 
 
 '''
-2. FLAG CREATION  (10PTS)
+2. FLAG CREATION  (10pts)
 ---------------
 Make your flag 260 pixels tall
 Use the scaling image on the website to determine other dimensions
