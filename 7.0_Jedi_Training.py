d3eca55e207fedb79d2fa4511c921d654b8544bc
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-10 08:55:11 -0500

    Update 7.0_Jedi_Training.py

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -1,7 +1,21 @@
-#Sign your name:________________
+# 7.0 Jedi Training (20pts)  Name:________________
 
 '''
+1. TEST PICTURE  (10PTS)
+------------------------
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
 '''
+
+
+'''
+2. FLAG  (10PTS)
+------------------------
+Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
+BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
+The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
+'''
+
+
+
