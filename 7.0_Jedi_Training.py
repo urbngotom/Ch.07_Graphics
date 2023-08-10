f8a3c4d8514d24d469dd9f77bf6f0190463822eb
Marc Hermon <hermonm@urbandaleschools.com>
2023-08-10 08:56:50 -0500

    Update 7.0_Jedi_Training.py

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -10,11 +10,16 @@ The picture is 500px wide and 400px tall. Look up ARC in the documentation to do
 
 
 '''
-2. FLAG  (10PTS)
-------------------------
-Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
-BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
-The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
+2. FLAG PROJECT  (10PTS)
+---------------
+Make your flag 260 pixels tall
+Use the scaling image on the website to determine other dimensions
+The hexadecimal colors for the official flag are red: #BF0A30 and blue: #002868
+Title the window, "The Stars and Stripes"
+You can use a draw_text command and used 20 pt. asterisks for the stars.
+We will have a competition to see who can make this flag in the least lines of code.
+The record is 16! You will have to use some loops to achieve this.
+CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
 '''
 
 
