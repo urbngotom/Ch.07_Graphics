f59ff857137e400502b4788532abf70bdc410680
Marc Hermon <hermonm@urbandaleschools.com>
2023-11-07 15:39:41 -0600

    Update 7.0_Jedi_Training.py

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -6,6 +6,7 @@
 Recreate, exactly the Test Picture from the website. The arcade colors used in this picture in no particular order are:
 BLACK, ALMOND, PHLOX, BLUSH, RED, BLUE, WISTERIA, AMBER, BRICK_RED and YELLOW.
 The picture is 500px wide and 400px tall. Look up ARC in the documentation to do the PAC-MAN.
+Upload your code and a screenshot of your picture.
 '''
 
 
@@ -17,9 +18,9 @@ Use the scaling image on the website to determine other dimensions
 The hexadecimal colors for the official flag are red: #BF0A30 and blue: #002868
 Title the window, "The Stars and Stripes"
 You can use a draw_text command and used 20 pt. asterisks for the stars.
-We will have a competition to see who can make this flag in the least lines of code.
-The record is 16! You will have to use some loops to achieve this.
+Can you use <20 lines of code?
 CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
+Upload your code and a screenshot of your flag.
 '''
 
 
